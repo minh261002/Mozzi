fbbd2eea7f8fe1de69d1a7505cd60c70919b48bf
Minh Tran <trcongminh.261002@gmail.com>
2026-01-20 11:44:02 +0700

    feat: add Permission and Role models with their many-to-many relationships

---
+++ app/Models/Permission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $table = 'permissions';
+
+    protected $guarded = [];
+
+    protected $casts = [];
+
+    /**
+     * Xác định mối quan hệ many-to-many với bảng roles.
+     *
+     * @return BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_has_permissions');
+    }
+}
